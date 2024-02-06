8e241f25cac93a38a6e0c14a9bc2787dc0d2435e
José Quiñónez <joserobertoq@outlook.com>
2024-02-06 04:01:33 -0600

    Se agregó la interfaz ICategoriaRepositorio

--- PropiedadesBlazor/Repositorio/IRepositorio/ICategoriaRepositorio.cs
+++ PropiedadesBlazor/Repositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -6,6 +6,18 @@ namespace PropiedadesBlazor.Repositorio.IRepositorio
 	public interface ICategoriaRepositorio
 	{
         public Task<IEnumerable<CategoriaDTO>> GetAllCategorias();
+
+        public Task<CategoriaDTO> GetCategoria(int categoriaId);
+
+        public Task<CategoriaDTO> CreatCategpria(CategoriaDTO categoriaDTO);
+
+        public Task<CategoriaDTO> ActualizarCategoria(int cateogiraId, CategoriaDTO categoriaDTO);
+
+        public Task<CategoriaDTO> NombreCategoriaExiste(string nombre);
+
+        public Task<CategoriaDTO> BorrarCategoriaExiste(int categoriaId);
+
+        //public Task<IEnumerable<CategoriaDTO>> GetDropDownCategorias();
     }
 }
 
