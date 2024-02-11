f5e296f4e77f767c989ed80294698b8127c2b4a7
José Quiñónez <joserobertoq@outlook.com>
2024-02-11 06:42:14 -0600

    Se agregó método de extensión

---
+++ PropiedadesBlazor/Helpers/IJsHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.JSInterop;
+
+namespace PropiedadesBlazor.Helpers
+{
+	public static class IJsHelper
+	{
+		public static async ValueTask ToastrSuccess(this IJSRuntime JSRuntime, string message)
+		{
+			await JSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+		}
+
+        public static async ValueTask ToastrError(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
+
