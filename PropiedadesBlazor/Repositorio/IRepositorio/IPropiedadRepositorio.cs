47598b37f76e2f4fbf6ba4e1ee3ababa398a286c
José Quiñónez <joserobertoq@outlook.com>
2024-02-12 05:26:45 -0600

    Se creó la interfaz de propiedad repositorio

---
+++ PropiedadesBlazor/Repositorio/IRepositorio/IPropiedadRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PropiedadesBlazor.Modelos.DTO;
+
+namespace PropiedadesBlazor.Repositorio.IRepositorio
+{
+	public interface IPropiedadRepositorio
+	{
+        public Task<IEnumerable<PropiedadDTO>> GetAllPropiedades();
+
+        public Task<PropiedadDTO> GetPropiedad(int propiedadId);
+
+        public Task<PropiedadDTO> CrearPropiedad(PropiedadDTO propiedadDTO);
+
+        public Task<PropiedadDTO> ActualizarPropiedad(int propiedadId, PropiedadDTO propiedadDTO);
+
+        public Task<int> BorrarPropiedad(int propiedadId);
+
+        public Task<PropiedadDTO> NombrePropiedadExiste(string nombre);
+    }
+}
+
