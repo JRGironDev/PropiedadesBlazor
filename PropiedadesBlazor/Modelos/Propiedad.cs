b8904975133370c4454e70f42668714f39084f19
José Quiñónez <joserobertoq@outlook.com>
2024-02-12 04:48:18 -0600

    Se agregó el modelo de Propiedad

---
+++ PropiedadesBlazor/Modelos/Propiedad.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PropiedadesBlazor.Modelos
+{
+	public class Propiedad
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Nombre { get; set; }
+
+		[Required]
+        public string Descripcion { get; set; }
+
+		[Required]
+		public int Area { get; set; }
+
+		[Required]
+		public int Habitaciones { get; set; }
+
+		[Required]
+		public int Banios { get; set; }
+
+		[Required]
+		public int Parqueadero { get; set; }
+
+		[Required]
+		public double Precio { get; set; }
+
+		[Required]
+		public bool Activo { get; set; }
+
+		public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+		public DateTime FechaActualizacion { get; set; }
+
+	}
+}
+
