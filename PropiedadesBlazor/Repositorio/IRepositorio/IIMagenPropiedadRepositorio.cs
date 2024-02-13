ca9bfe22349a019b5f82ac8bac43b861e9e8033b
José Quiñónez <joserobertoq@outlook.com>
2024-02-13 08:54:36 -0600

    Se creó la interfaz del repositorio ImagenPropiedad

---
+++ PropiedadesBlazor/Repositorio/IRepositorio/IIMagenPropiedadRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PropiedadesBlazor.Modelos.DTO;
+
+namespace PropiedadesBlazor.Repositorio.IRepositorio
+{
+	public interface IIMagenPropiedadRepositorio
+	{
+		public Task<int> CrearPropiedadImagen(ImagenPropiedadDTO imagenDTO);
+
+		public Task<int> BorrarPropiedadImagenPorIdImagen(int imagenDTO);
+
+		public Task<int> BorrarPropiedadImagenPorIdPropiedad(int propiedadId);
+
+		public Task<int> BorrarPropiedadImagenPorUrlImagen(string imageUrl);
+
+		public Task<IEnumerable<ImagenPropiedadDTO>> GetImagenPropiedad(int propiedadId);
+	}
+}
+
