3daff246b85f354b48d479a557f1959ee8f5e8d8
José Quiñónez <joserobertoq@outlook.com>
2024-04-01 05:15:09 -0600

    Se creó interfaz de repositorio de subida de archivos

---
+++ PropiedadesBlazor/Servicios/ISubidaArchivos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace PropiedadesBlazor.Servicios
+{
+	public interface ISubidaArchivos
+	{
+		Task<string> SubirArchivo(IBrowserFile archivo);
+
+		bool BorrarArchivo(string nombreArchivo);
+	}
+}
+
